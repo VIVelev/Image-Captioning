d8ab473285a4c267326a1bf923c1c05e343cdeff
Victor Velev <velev.victor@yahoo.com>
2019-04-10 02:06:41 +0300

    [Fix] config path to data

--- src/utils/config.py
+++ src/utils/config.py
@@ -6,7 +6,7 @@ __all__ = [
 ]
 
 
-PATH_TO_DATA = '/Users/victor/Desktop/Image-Captioning/data/'
+PATH_TO_DATA = '../../data/'
 TEXT_FILES_DIR = PATH_TO_DATA + 'Flickr8k_text/'
 IMAGES_DIR = PATH_TO_DATA + 'Flickr8k_Dataset/Flicker8k_Dataset/'
 IMAGE_SIZE = (299, 299, 3)
